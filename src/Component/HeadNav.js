243c0d213e2172cbd4e42ac70baf22ca286e9755
vinit20 <vinitvicky0@gmail.com>
2024-03-04 17:19:59 +0530

    checking git setup with c-panel

--- src/Component/HeadNav.js
+++ src/Component/HeadNav.js
@@ -169,6 +169,7 @@ const HeadNav = () => {
                 >
                   Case Study
                 </a>
+                
               </div>
             </div>
             &nbsp;&nbsp;&nbsp;
